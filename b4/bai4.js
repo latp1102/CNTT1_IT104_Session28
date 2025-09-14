d4c1e7589a6fe2885d3cec20d0094576995ef4f9
La The Phong <latp1102@gmail.com>
2025-09-14 20:15:16 +0700

    update files

--- b4/bai4.js
+++ b4/bai4.js
@@ -6,7 +6,7 @@ const processArr = (arr, callback) => {
     });
 }
 const printElement = (element) => {
-    console.log(`Phần tử thứ: ${element}`);
+    console.log(`phần tử thứ: ${element}`);
 }
 const numbers = [1,2,3,4,5];
 processArr(numbers, printElement);
